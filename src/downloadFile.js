98b02e425d8ca93376bbac008af7de5239e0c092
Divyanshu <bali.divyanshu319@gmail.com>
2020-06-27 22:54:40 +0530

    Add case for single file download

--- src/downloadFile.js
+++ src/downloadFile.js
@@ -10,39 +10,74 @@ function downloadFile(start, downloadFiles, drive) {
     return
   }
 
-  const file = downloadFiles[start]
-  let progress = 0
-  const dest = fs.createWriteStream(`./downloads/${file.name}`)
+  if (!Array.isArray(downloadFiles)) {
+    const file = downloadFiles
+    let progress = 0
+    const dest = fs.createWriteStream(`./downloads/${file.name}`)
 
-  drive.files
-    .get(
-      {
-        fileId: file.id,
-        alt: 'media'
-      },
-      {
-        responseType: 'stream'
-      }
-    )
-    .then((res) => {
-      console.log()
-      progBar.start(file.size, 0)
-      res.data
-        .on('end', () => {
-          progBar.stop()
-          console.log(`${file.name} downloaded.`)
-          downloadFile(start + 1, downloadFiles, drive)
-        })
-        .on('error', () => {
-          progBar.stop()
-          console.log(`Error occurred while downloading ${file.name}`)
-        })
-        .on('data', (data) => {
-          progress += data.length
-          progBar.update(progress)
-        })
-        .pipe(dest)
-    })
+    drive.files
+      .get(
+        {
+          fileId: file.id,
+          alt: 'media'
+        },
+        {
+          responseType: 'stream'
+        }
+      )
+      .then((res) => {
+        console.log()
+        progBar.start(file.size, 0)
+        res.data
+          .on('end', () => {
+            progBar.stop()
+            console.log(`${file.name} downloaded.`)
+          })
+          .on('error', () => {
+            progBar.stop()
+            console.log(`Error occurred while downloading ${file.name}`)
+          })
+          .on('data', (data) => {
+            progress += data.length
+            progBar.update(progress)
+          })
+          .pipe(dest)
+      })
+  } else {
+    const file = downloadFiles[start]
+    let progress = 0
+    const dest = fs.createWriteStream(`./downloads/${file.name}`)
+
+    drive.files
+      .get(
+        {
+          fileId: file.id,
+          alt: 'media'
+        },
+        {
+          responseType: 'stream'
+        }
+      )
+      .then((res) => {
+        console.log()
+        progBar.start(file.size, 0)
+        res.data
+          .on('end', () => {
+            progBar.stop()
+            console.log(`${file.name} downloaded.`)
+            downloadFile(start + 1, downloadFiles, drive)
+          })
+          .on('error', () => {
+            progBar.stop()
+            console.log(`Error occurred while downloading ${file.name}`)
+          })
+          .on('data', (data) => {
+            progress += data.length
+            progBar.update(progress)
+          })
+          .pipe(dest)
+      })
+  }
 }
 
 module.exports = downloadFile
